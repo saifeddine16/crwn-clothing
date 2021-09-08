6226e173546a97d4322cbb824a008488b29834d9
saifeddine16 <bellakhalsaif1996@gmail.com>
2021-09-08 14:42:53 +0100

    create the shop page and the coleection preview components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
 import './App.css';
 
 const HatsPage = () => (
@@ -14,7 +18,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component= {HomePage} />
-        <Route path='/Hats' component= {HatsPage} />
+        <Route path='/shop' component= {ShopPage} />
       </Switch>
     </div>
   );
