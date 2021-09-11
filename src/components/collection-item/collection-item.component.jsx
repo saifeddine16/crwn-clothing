92cd3782dbd2857a28399a72626c8d878cd0a78e
saifeddine16 <bellakhalsaif1996@gmail.com>
2021-09-11 23:32:15 +0100

    updated shop data with new data, updated cart utilsto group items in cart reducer, and modified some styles

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import Custombutton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({id, name, price, imageUrl}) =>(
-    <div className='collection-item'>
-        <div
-            className='image'
-            style= {{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className='collection-footer'>
-            <span className='name'>{ name }</span>
-            <span className='price'>{ price }</span>
-        </div>    
-    </div>
-);
+const CollectionItem = ({item, addItem}) =>{
+    const { name, price, imageUrl } = item;
+    return(
+        <div className='collection-item'>
+            <div
+                className='image'
+                style= {{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className='collection-footer'>
+                <span className='name'>{ name }</span>
+                <span className='price'>{ price }</span>
+            </div>    
+            <Custombutton onClick={() => addItem(item)} inverted>
+                Add to cart
+            </Custombutton>
+        </div>
+    )
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import './custom-button.styles.scss';
 
-const Custombutton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in': '' } custom-button`} {...otherProps} > 
+const Custombutton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+    <button className={`${inverted ? 'inverted': '' } custom-button`} {...otherProps} > 
         {children}
     </button>
 );
